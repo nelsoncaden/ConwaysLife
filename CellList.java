3007f0bd9a5b4fe8f80bcabfba60a993643f5f9c
Caden Nelson <nelsonck@miamioh.edu>
2022-09-23 19:12:09 -0400

    Entering the basic starter files

---
+++ CellList.java
@@ -0,0 +1,15 @@
+/**
+ * Forms a data structure for the cells.
+ * @author Caden Nelson
+ *
+ */
+public class CellList {
+	
+	public CellList(int rectSize) {
+		for (int i = 0; i < rectSize; i++) {
+			for (int j = 0; j < rectSize; j++) {
+			}
+		}
+	} 
+
+}
---
+++ Driver.java
@@ -0,0 +1,17 @@
+/**
+ * Presents user with grid on which they may click to change any cell states
+ * they would like to living. Once satisfied, user must click start and watches
+ * how the cell states evolve according to Conway's Game of Life rules. This 
+ * class will contain a loop which iterates through each step evolving the 
+ * game.
+ * @author Caden Nelson
+ *
+ */
+public class Driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
